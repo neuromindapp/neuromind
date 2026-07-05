aea9c6382e74fd7b2184a6d8fec848162d7f16cd
neuromindapp <147643724+neuromindapp@users.noreply.github.com>
2026-07-05 08:19:00 +0200

    Add frontend App

---
+++ frontend/src/App.tsx
@@ -0,0 +1,32 @@
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { Navbar } from './components/Navbar'
+import { Footer } from './components/Footer'
+import Landing from './pages/Landing'
+import Edges from './pages/Edges'
+import TrackRecord from './pages/TrackRecord'
+import Account from './pages/Account'
+import Report from './pages/Report'
+
+export function App() {
+  const { pathname } = useLocation()
+  const isLanding = pathname === '/'
+
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <main className={isLanding ? '' : 'app-page min-h-screen'}>
+        <div className={isLanding ? '' : 'flex min-h-[60vh] flex-col'}>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/edges" element={<Edges />} />
+            <Route path="/track-record" element={<TrackRecord />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/report/:id" element={<Report />} />
+          </Routes>
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
